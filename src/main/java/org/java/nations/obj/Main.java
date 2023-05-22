d416615258db98f62e7c0c0feec938a707afa805
SamuelePassiatore <semmisno2@protonmail.com>
2023-05-22 16:38:38 +0200

    milestone 3 done

--- src/main/java/org/java/nations/obj/Main.java
+++ src/main/java/org/java/nations/obj/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,6 +15,10 @@ public class Main {
 			String password = "root";
 			
 				try(Connection con = DriverManager.getConnection(url, user, password)) {
+					
+					 Scanner scanner = new Scanner(System.in);
+			         System.out.println("Inserisci una stringa di ricerca: ");
+			         String searchString = scanner.nextLine();
 			    
 					String sql = "SELECT countries.name, countries.country_id, regions.name, continents.name "
 						+ "FROM countries "
@@ -21,15 +26,18 @@ public class Main {
 						+ "ON countries.region_id = regions.region_id "
 					    + "JOIN continents "
 						+ "ON regions.continent_id = continents.continent_id "
+						+ "WHERE countries.name LIKE ? " 
 						+ "ORDER BY countries.name ";
 				
 				
 					try (PreparedStatement ps = con.prepareStatement(sql)) {
+						ps.setString(1, "%" + searchString + "%");
 											
 						try (ResultSet rs = ps.executeQuery()) {
 							
 							System.out.println("Nazione - ID - Regione - Continente");
 							
+						    boolean hasResults = false;
 							while(rs.next()) {
 								
 								final String countryName = rs.getString(1);
@@ -39,17 +47,23 @@ public class Main {
 								
 								System.out.println(countryName + " - " + countryId + " - " 
 										+ regionName + " - " + continentName);
+								
+								hasResults = true;
+							
 							}
+							if (!hasResults) {
+		                        System.out.println("Nessuna nazione trovata corrispondente alla ricerca.");
+		                   }
 					   } catch (SQLException ex) {
-						System.err.println(ex);
+						System.err.println("Error");
 					   }
 						
 					} catch (SQLException ex) {
-						System.err.println(ex);
+						System.err.println("Query Error");
 					}
 
 			} catch (SQLException ex){
-				System.err.println(ex);
+				System.err.println("Connection error");
 			}
 	}
 }
